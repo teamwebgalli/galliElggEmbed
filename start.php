ad31a15589baebeb8deb4024062cc7eab7436961
Team Webgalli <webgalli@gmail.com>
2016-08-02 07:47:17 +0530

    Revert "Revert "Updates the plugin for Elgg 1.9+""

--- start.php
+++ start.php
@@ -9,16 +9,14 @@
  *	Licence : GPLV2
  *	Copyright : Team Webgalli 2013-2015
  */
- 
+
 
 elgg_register_event_handler('init', 'system', 'galliElggEmbed_init');
 
 function galliElggEmbed_init() {
-	elgg_extend_view('js/elgg', 'galliElggEmbed/js');
 	elgg_extend_view('page/elements/sidebar', 'galliElggEmbed/share', 1000);
-	
+
+	elgg_require_js("galliElggEmbed");
+
 	elgg_register_viewtype_fallback("embed");
-	elgg_register_simplecache_view("js/galliElggEmbed/embed");
-	$js_url = elgg_get_simplecache_url("js", "galliElggEmbed/embed");
-	elgg_register_js("galliElggEmbed", $js_url);
 }
